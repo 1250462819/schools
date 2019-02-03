eba5f13fef26e04e3454a3b0c35536e2f3f744a3
badrex <beheshti.7676@gmail.com>
2019-02-03 04:22:46 -0800

    edit index

--- app/Http/Controllers/Home/PostController.php
+++ app/Http/Controllers/Home/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Address;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,7 +10,6 @@ class PostController extends Controller
 {
     public function index($name){
         $addresses=Address::all()->where('school.name','=',$name);
-
-        return view('Home.school',compact('name','addresses'));
+        return view('Home.school',compact('name'));
     }
 }
