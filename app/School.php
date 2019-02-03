be0dcd6a5f5fec55c841b70e101e76bf9d853d84
badrex <beheshti.7676@gmail.com>
2019-02-03 07:07:38 -0800

    ddynamic post

--- app/School.php
+++ app/School.php
@@ -37,6 +37,10 @@ class School extends Model
     {
         return $this->hasOne(Slider::class);
     }
+    public function post()
+    {
+        return $this->hasOne(Post::class);
+    }
     public function classrooms()
     {
         return $this->hasMany(Classroom::class);
